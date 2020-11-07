dc99788cdd2e60f16037a4589c01af8f50281583
Alberto Gonzalez <>
2020-11-07 20:57:41 +0100

    Se ha creado la estructura de la cabecera

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-app-bar app dark>
+    <div class="d-flex ma-3">
+      LOGO
+    </div>
+    <div class="d-flex align-center">
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn class="ml-2" small color="success" v-bind="attrs" v-on="on">
+            <v-icon small class="pr-1 icons">
+              mdi-view-grid-plus
+            </v-icon>
+            Crear
+          </v-btn>
+        </template>
+        <span>Crear nuevo componente</span>
+      </v-tooltip>
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn class="ml-2" small color="success" v-bind="attrs" v-on="on">
+            <v-icon small class="pr-1 icons">
+              mdi-content-save
+            </v-icon>
+            Guardar
+          </v-btn>
+        </template>
+        <span>Guardar proyecto</span>
+      </v-tooltip>
+    </div>
+    <v-spacer></v-spacer>
+    <div class="d-flex align-center">
+      <v-select
+        class="pr-3 pt-5 mt-1"
+        v-model="select"
+        append-icon="mdi-translate"
+        :items="items"
+        item-text="name"
+        item-value="id"
+        label="Seleccionar idioma"
+        dense
+        solo
+        return-object
+      ></v-select>
+      <v-badge
+        class="mr-4"
+        :content="messages"
+        :value="messages"
+        color="red"
+        overlap
+      >
+        <v-icon class="cpointer">
+          mdi-bell
+        </v-icon>
+      </v-badge>
+      <MyProfileOptionsComponent />
+    </div>
+  </v-app-bar>
+</template>
+<script>
+import MyProfileOptionsComponent from "./MyProfileOptionsComponent";
+export default {
+  name: "HeaderComponent",
+  components: {
+    MyProfileOptionsComponent,
+  },
+  data() {
+    return {
+      darkMode: false,
+      messages: 1,
+      items: [
+        { id: 1, name: "Español" },
+        { id: 2, name: "Català" },
+        { id: 3, name: "English" },
+        { id: 4, name: "Française" },
+      ],
+    };
+  },
+  methods: {},
+};
+</script>
---
+++ src/components/MyProfileOptionsComponent.vue
@@ -0,0 +1,87 @@
+<template>
+  <v-menu
+    :close-on-content-click="false"
+    bottom
+    min-width="200px"
+    rounded
+    offset-y
+  >
+    <template v-slot:activator="{ on }">
+      <v-avatar class="cpointer" color="brown" size="36" v-on="on">
+        <span class="white--text">AG</span>
+      </v-avatar>
+    </template>
+    <v-card max-width="300" class="mx-auto pa-3">
+      <div class="d-flex align-center pb-3">
+        <img
+          class="profile-icon"
+          src="https://cdn.vuetifyjs.com/images/john.jpg"
+          width="72"
+        />
+        <div class="d-flex flex-column pl-3">
+          <v-chip class="membership" small color="primary">
+            miembro standard
+          </v-chip>
+          <h5 class="pt-2">Alberto González Ferrer</h5>
+          <small>alberto2daw@gmail.com</small>
+        </div>
+      </div>
+      <v-divider></v-divider>
+      <v-list dense>
+        <v-list-item
+          class="cpointer list-wrapper"
+          :class="item.disabled ? 'disabled' : ''"
+          v-for="(item, i) in items"
+          :key="i"
+          :disabled="item.disabled"
+        >
+          <v-list-item-icon>
+            <v-icon v-text="item.icon"></v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-subtitle v-text="item.text"></v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+      <v-divider></v-divider>
+      <div class="d-flex justify-center align-center">
+        <label class="subtitle-2 pr-3">Modo oscuro: </label>
+        <v-switch v-model="darkMode" color="info"></v-switch>
+      </div>
+    </v-card>
+  </v-menu>
+</template>
+<script>
+export default {
+  name: "MyProfileOptions",
+  data() {
+    return {
+      items: [
+        { text: "Mi perfil", icon: "mdi-account", disabled: false },
+        { text: "Mis proyectos", icon: "mdi-archive", disabled: false },
+        { text: "Hazte premium", icon: "mdi-star-check", disabled: true },
+        { text: "Cerrar sesión", icon: "mdi-power", disabled: false },
+      ],
+    };
+  },
+};
+</script>
+<style lang="scss" scoped>
+.profile-icon {
+  background-size: cover;
+  border-radius: 100% !important;
+}
+.list-wrapper:hover {
+  background-color: #efefef;
+}
+.membership {
+  font-size: 8px !important;
+  max-width: 100px;
+  height: 20px !important;
+  align-self: flex-end;
+  font-weight: bolder;
+}
+.v-switch >>> .v-label {
+  font-size: 14px !important;
+}
+</style>
